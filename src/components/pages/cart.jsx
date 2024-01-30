9b020d5a19fbdd80355aee9a5fdbc213b60a3126
Srinivasan <15118@ramco.com>
2024-01-30 12:05:06 +0530

    first

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from '../style/cart.module.css';
 
 function Cart({ cart, setCart }) {
